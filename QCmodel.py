0eac95df54fdd6459c76dd3efe6c58dfa6215300
MiaLiu123 <148765247+MiaLiu123@users.noreply.github.com>
2023-12-14 23:10:55 +0100

    Update QCmodel.py

--- QCmodel.py
+++ QCmodel.py
@@ -1,43 +1,116 @@
-# PERSON (29, 30, 31)
-# NORP (Nationalities/religious/political groups) NORP （国籍/宗教/政治团体）(16)
-# ORG (Organization) ORG （组织）(28)
-# GPE (Countries/cities/states) GPE （国家/城市/州）(32,33,36)
-# LOC (Location) LOC （位置）(34, 35)
-# FAC, PRODUCT, WORK_OF_ART, LAW(2,3,5,9,10,14,15,22)
-# EVENT (8)
-# LANGUAGE (11)
-# DATE (39)
-# PERCENT(45)
-# MONEY(41)
-# QUANTITY(48, 49, 40)
-# ORDINAL(42)
-# CARDINAL (others), TIME ()
+# 0 PERSON (29, 30, 31)
 
+# 1 NORP (Nationalities/religious/political groups) NORP （国籍/宗教/政治团体）(16)
+
+# 2 ORG (Organization) ORG （组织）(28)
+
+# 3 GPE (Countries/cities/states) GPE （国家/城市/州）(32,33,36)
+
+# 4 LOC (Location) LOC （位置）(34, 35)
+
+# 5 FAC, PRODUCT, WORK_OF_ART, LAW(2,3,5,9,10,14,15,22)
+
+# 6 EVENT (8)
+
+# 7 LANGUAGE (11)
+
+# 8 DATE (39)
+
+# 9 PERCENT(45)
+
+# 10 MONEY(41)
+
+# 11 QUANTITY(48, 49, 40)
+
+# 12 ORDINAL(42)
+
+# 13 TIME, CARDINAL (others)(42)
+import nltk
+from nltk.corpus import nps_chat
 import torch
-from transformers import BertModel, BertTokenizer, BertConfig, AutoModelForSequenceClassification, AutoTokenizer
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-MODEL_NAME = 'bert-base-cased'
-MODEL_PATH = 'model/transformers' + "/" + MODEL_NAME
-
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+posts = nps_chat.xml_posts()[:5000]
 
 
-def classify(question):
+def dialogue_act_features(post):
+    features = {}
+    for word in nltk.word_tokenize(post):
+        features['contains({})'.format(word.lower())] = True
+    return features
+
+
+# identify yes/no questions
+def is_yes_no_question(question):
+    # 首先使用nltk的分类器
+    featuresets = [(dialogue_act_features(post.text), post.get('class')) for post in posts]
+    size = int(len(featuresets) * 0.1)
+    train_set, test_set = featuresets[size:], featuresets[:size]
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
+    # print(nltk.classify.accuracy(classifier, test_set))
+    return classifier.classify(dialogue_act_features(question))
+
+#如果是yes/no问题，直接返回结果, 否则使用bert模型
+def Bert(text):
+    MODEL_NAME = 'bert-base-cased'
+    MODEL_PATH = 'model/transformers' + "/" + MODEL_NAME
+
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+
     # 加载tokenizer
     tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")  # 或者您使用的其他模型
 
     # 准备输入数据
-    # question = "When is Chrismas day"  # modify here
-    inputs = tokenizer(question, padding="max_length", truncation=True, max_length=128, return_tensors="pt")
+    inputs = tokenizer(text, padding="max_length", truncation=True, max_length=128, return_tensors="pt")
+
     # 生成预测
     with torch.no_grad():
         outputs = model(**inputs)
         predictions = torch.argmax(outputs.logits, dim=-1)
+    entity_label = entity_labels_transfer(predictions.item())
 
-    return predictions.item()
-# print("predict labels:", predictions.item())
+    return ("entity labels:", entity_label)
+
+def entity_labels_transfer(label):
+    if label == 0:
+        return "PERSON"
+    elif label == 1:
+        return "NORP"
+    elif label == 2:
+        return "ORG"
+    elif label== 3:
+        return "GPE"
+    elif label== 4:
+        return "LOC"
+    elif label== 5:
+        return "FAC, PRODUCT, WORK_OF_ART, LAW"
+    elif label== 6:
+        return "EVENT"
+    elif label== 7:
+        return "LANGUAGE"
+    elif label== 8:
+        return "DATE"
+    elif label== 9:
+        return "PERCENT"
+    elif label== 10:
+        return "MONEY"
+    elif label== 11:
+        return "QUANTITY"
+    elif label== 12:
+        return "ORDINAL"
+    elif label== 13:
+        return "TIME, CARDINAL (others)"
 
 
+def question_classification(text):
+    result = is_yes_no_question(text)
+    if result == 'ynQuestion':
+        # print(result)
+        return result
+    else:
+        result = Bert(text)
+        # print(result)
+        return result
 if __name__ == "__main__":
     # simple questions, perform well
     q1 = "Is Rome the capital of Italy?"
@@ -71,7 +144,17 @@ if __name__ == "__main__":
 
     questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19,q20,q21,q22,q23,q24]
 
-    for q in questions:
-        # 生成预测
-        label = classify(q)
-        print(q, label)
+# test
+for q in questions:
+    # 生成预测
+    result = question_classification(q)
+    print(q, result)
+
+
+# train_set = []
+# for post in posts:
+#     if post.get('class') == 'whQuestion' or post.get('class') == 'ynQuestion':
+#        train_set.append((post.text, post.get('class')))
+
+
+# print(train_set)
