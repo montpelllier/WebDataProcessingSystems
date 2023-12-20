fe3fcd452be76a9f8a5c214f16a622dbb89ba083
ifon <1565975426@qq.com>
2023-12-20 02:10:21 +0100

    debug

--- assignment/html_parser.py
+++ assignment/html_parser.py
@@ -14,14 +14,19 @@ def parse_url(url):
 
 def get_wikipedia_url(soup):
     content = soup.find('span', {'class': 'wikibase-sitelinkview-link wikibase-sitelinkview-link-enwiki'})
-    content = content.find('span', {'class': 'wikibase-sitelinkview-page'}).find('a')
+    if not content:
+        return None
 
+    content = content.find('span', {'class': 'wikibase-sitelinkview-page'}).find('a')
     wikipedia_url = content.get('href')
+
     return wikipedia_url
 
 
 def get_wikidata_brief(soup):
     content = soup.find('div', {'class': "wikibase-entitytermsview-heading-description"})
+    if not content:
+        return None
     return content.get_text()
 
 
@@ -36,17 +41,22 @@ def get_wikipedia_page_content(soup):
     return paragraph_texts
 
 
-# # 维基百科页面的示例链接
-# wikipedia_url = "https://en.wikipedia.org/wiki/apple"
-# # 获取并解析页面内容
-# page_content = get_wikipedia_page_content(wikipedia_url)
-# # 打印页面内容
-# for p in page_content:
-#     print(p.replace("\n", ""))
-# url = "https://www.wikidata.org/wiki/Q47740"
-# # url = "https://www.wikidata.org/wiki/Q2781527"
-# soup = parse_url(url)
-# if soup:
-#     wikipedia_url = get_wikipedia_url(soup)
-#     brief = get_wikidata_brief(soup)
-#     print(wikipedia_url, brief)
+if __name__ == "__main__":
+
+    # # 维基百科页面的示例链接
+    # wikipedia_url = "https://en.wikipedia.org/wiki/apple"
+    # # 获取并解析页面内容
+    # page_content = get_wikipedia_page_content(wikipedia_url)
+    # # 打印页面内容
+    # for p in page_content:
+    #     print(p.replace("\n", ""))
+    # url = "https://www.wikidata.org/wiki/Q47740"
+
+    # url = "https://www.wikidata.org/wiki/Q2781527"
+    url = "http://www.wikidata.org/wiki/Q209878"
+    soup = parse_url(url)
+    # print(soup)
+    if soup:
+        wikipedia_url = get_wikipedia_url(soup)
+        brief = get_wikidata_brief(soup)
+        print(wikipedia_url, brief)
