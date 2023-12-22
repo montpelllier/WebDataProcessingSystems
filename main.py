c72949a3052ed99de0eac25a494bb2be844c7bc3
MiaLiu123 <mialiu755@gmail.com>
2023-12-22 13:31:34 +0100

    main update

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 from QCmodel import question_classification
 from fact_checking import fact_checking
 from example_using_llm import get_completion
+from assignment.answer_extractor import get_entities
+from entity_linking import link_entity
+import stanza
 def main():
 
+# generate question and answer
     question = input("Type your question and type ENTER to finish:\n")
     answer = get_completion(question)
-    # entity_linking
-    questionclassify = question_classification(question)
-    # factchecking
+
+# entity_linking
+    # entity_linking = entity_linking(question, answer)
+    # questionclassify = question_classification(question)
+
+# factchecking
+    q_doc = trans_to_doc(question)
+    entity_question = get_entities(q_doc)
+    entity_question_link = link_entity(q_doc, entity_question)
+    # test
+    # print("question", question)
+    # print("q_doc", q_doc)
+    # print("question entity",entity_question)
+    print("question entity link",entity_question_link)
+
     # factcheck = fact_checking(question, entity_question, entity_question_link, extracted_answer)
-    print("answer", answer)
-    print("question classify:", questionclassify)
+    # print("answer", answer)
+    # print("question classify:", questionclassify)
     # print("factchecking",factcheck)
 
+# 
+
+
+
+def trans_to_doc(ques):
+    nlp = stanza.Pipeline(lang='en', processors='tokenize,ner,mwt,pos,lemma,sentiment', download_method=None)
+    q_doc = nlp(ques)
+    return q_doc
+    
 if __name__ == "__main__":
     main()
