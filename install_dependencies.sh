60002a463b32ca7cd667bd56c0d9519a68bf22cf
kokonot8 <144746065+kokonot8@users.noreply.github.com>
2023-12-22 21:38:45 +0100

    tested on docker

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# 确保脚本在遇到错误时终止
-set -e
-
-## 更新pip到最新版本
-#pip install --upgrade pip
-
-# 安装requirements.txt中的所有依赖
-pip install -r requirements.txt
-
+REQUIREMENTS_FILE="requirements.txt"
+pip install -r $REQUIREMENTS_FILE
 echo "Sucessfully install all dependencies!"
