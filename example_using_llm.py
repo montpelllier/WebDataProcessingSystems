c6eb8c6f2630d4fbeeb637fba1b18631cd47ca18
ifon <1565975426@qq.com>
2023-12-23 19:53:20 +0100

    llm

--- example_using_llm.py
+++ example_using_llm.py
@@ -1,14 +1,15 @@
 from ctransformers import AutoModelForCausalLM
+
 repo = "TheBloke/Llama-2-7B-GGUF"
 model_file = "llama-2-7b.Q4_K_M.gguf"
 llm = AutoModelForCausalLM.from_pretrained(repo, model_file=model_file, model_type="llama")
 
-def get_completion(prompt):
-	while(True):
-		if prompt == 'quit':
-			break
-		print("Computing the answer (takes time)...")
-		completion = llm(prompt)
-		# print("COMPLETION: %s" % completion)
-		return completion
 
+def get_completion(prompt):
+    while True:
+        if prompt == 'quit':
+            break
+        print("Computing the answer (takes time)...")
+        completion = llm(prompt)
+        # print("COMPLETION: %s" % completion)
+        return completion
